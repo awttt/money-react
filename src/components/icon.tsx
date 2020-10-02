502d842531bf6a6fe84127d8cc742956528de234
awttt <350944364@qq.com>
2020-10-03 01:12:53 +0800

    解决 tag icon 选中动作问题

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {MouseEventHandler} from 'react';
+
 
 type Props = {
   name:string
+  onClick?: MouseEventHandler
+  className?:string
 }
+
+
 
 const Icon = (props:Props)=>{
   return(
-  <svg className="icon">
+  <svg className={`icon ${props.className}`}
+       onClick={props.onClick}
+
+  >
     <use xlinkHref={'#'+props.name}/>
   </svg>
+
   )
 }
 
--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
+import Icon from '../../components/icon';
 
-const TagsSection = styled.section`
+
+
+
+
+const Wrapper = styled.section`
   background: #FFFFFF; padding: 12px 16px;
   flex-grow: 1; display:flex; flex-direction: column;
   justify-content: flex-end; align-items: flex-start;
@@ -10,6 +16,11 @@ const TagsSection = styled.section`
        display:inline-block; padding: 3px 18px; 
        font-size: 14px; margin: 8px 12px;
     }
+    > svg{
+     &.selected{
+       fill: red;
+       }
+    }
   }
   > button{
     background:none; border: none; padding: 2px 4px;
@@ -17,5 +28,42 @@ const TagsSection = styled.section`
     margin-top: 8px;
   }
 `;
+
+
+const TagsSection:React.FC = (props)=>{
+  const [tags,setTags]=useState<string[]>(['衣','食','行'])
+  const [selectedTags,setSelectedTags] = useState<string[]>([])
+  const onAddTag =()=>{
+   const tagName = window.prompt('新标签的名称为')
+    if (tagName!==null){
+      setTags([...tags,tagName])
+    }
+  }
+  const onToggleTag=(tag:string)=>{
+    const index = selectedTags.indexOf(tag)
+    if (index>=0){
+      setSelectedTags(selectedTags.filter(t=>t!==tag))
+      console.log(tag);
+    }else{
+      setSelectedTags([...selectedTags,tag])
+      console.log(11111);
+    }
+  }
+  return(
+    <Wrapper>
+      <ol>
+        {tags.map((tag,key)=>
+        <Icon name={tag} key={key}
+              onClick={()=>onToggleTag(tag)}
+              className={selectedTags.indexOf(tag)>=0 ? 'selected' : ''}
+        />
+        )}
+      </ol>
+      <button onClick={onAddTag} >新增标签</button>
+    </Wrapper>
+  )
+}
+
+
 
 export {TagsSection};
